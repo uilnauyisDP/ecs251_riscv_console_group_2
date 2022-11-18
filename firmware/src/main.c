6dd80ef8f92e0ee8651c3327766a79143af928c4
ZixinYang <zixinyang92@gmail.com>
2022-11-18 08:53:50 -0800

    Add InitContext and SwitchContext

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,10 +5,25 @@ volatile uint32_t controller_status = 0;
 volatile uint32_t *CARTRIDGE = (volatile uint32_t *)(0x4000001C);
 volatile char *VIDEO_MEMORY = (volatile char *)(0x50000000 + 0xFE800);
 typedef void (*FunPtr)(void);
+typedef uint32_t *TContext;
+typedef void (*TEntry)(void *);
+
+TContext InitContext(uint32_t *stacktop, TEntry entry, void *param);
+void SwitchContext(TContext *old, TContext new);
+
+TContext MainThread;
+TContext OtherThread;
+
+void Thread(void *param){
+    ((FunPtr)((*CARTRIDGE) & 0xFFFFFFFC))();
+    SwitchContext(&OtherThread,MainThread);
+}
 int main() {
+    uint32_t ThreadStack[128];
+    OtherThread = InitContext(ThreadStack + 128,Thread,(void *)0);
     while(1){
         if((*CARTRIDGE) & 0x1){
-            ((FunPtr)((*CARTRIDGE) & 0xFFFFFFFC))();
+            SwitchContext(&MainThread,OtherThread);
         }
     }
     return 0;

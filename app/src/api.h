c591fd8ee94f3203d43ca704e58324bd2764b13e
ZixinYang <zixinyang92@gmail.com>
2022-11-18 16:43:53 -0800

    ProjectPhase3

--- app/src/api.h
+++ app/src/api.h
@@ -8,10 +8,3 @@ uint32_t calcLargeSpriteControl(uint32_t x, uint32_t y, uint32_t w, uint32_t h, 
 uint32_t calcBackgroundControl(uint32_t x, uint32_t y, uint32_t z, uint32_t p);
 void setGraphicsMode(void);
 void setTextMode(void);
-
-
-volatile int global = 42;
-volatile uint32_t controller_status = 0;
-volatile char *VIDEO_MEMORY = (volatile char *)(0x500FE800);
-volatile uint32_t *SMALL_SPRITE_CONTROLS[128];
-volatile uint8_t *SMALL_SPRITE_DATAS[128];

bfd9ebf3083c9e130fd128c4b2b256e56f96175c
Siyuan Liu <sssiu@ucdavis.edu>
2022-12-04 16:08:17 -0800

    Refactor bird move

--- birdApp/src/main.c
+++ birdApp/src/main.c
@@ -18,60 +18,57 @@ void initVideoSetting();
 int main()
 {
     initVideoSetting();
-    
-    threadGraphics();
-}
 
-void threadGraphics()
-{
-    struct position flappyBird;
-    flappyBird.y = 30;
-    flappyBird.x = 30;
-
-    int x_pos = 18;
-    int sprite_inc_x;
-    int sprite_inc_y;
-    int move_speed = 5;
-    uint32_t time_counter = 0;
+    int x_pos = 30;
+    int y_pos = 30;
     uint32_t last_time = 0;
+
     while (running_flag)
     {
         global = getTicks();
-
-        sprite_inc_x = 0;
-        sprite_inc_y = 0;
         if (global != last_time)
         {
-            //linePrintf(0, "video interrupt: %d", getVideoInterruptSeq());
             controller_status = getStatus();
             if (controller_status)
             {
-                //linePrintf(5, "controller status: %d", controller_status);
-                if (controller_status & 0x1)
-                {
-                    sprite_inc_x = -move_speed;
-                }
-                if (controller_status & 0x2)
-                {
-                    sprite_inc_y = -move_speed;
-                }
-                if (controller_status & 0x4)
-                {
-                    sprite_inc_y = move_speed;
-                }
-                if (controller_status & 0x8)
-                {
-                    sprite_inc_x = move_speed;
-                }
+                threadGraphics(&x_pos, &y_pos);
             }
-            flappyBird.x += sprite_inc_x;
-            flappyBird.y += sprite_inc_y;
-            for (int i = 0; i < 3; i++)
-            {
-                setLargeSpriteControl(i, 64, 64, flappyBird.x, flappyBird.y, i == global % 3);
-            }
-            last_time = global;
         }
-        //threadYield();
+        last_time = global;
     }
+
+    //threadGraphics();
+}
+
+void threadGraphics(int *x_pos, int *y_pos)
+{
+    int sprite_inc_x;
+    int sprite_inc_y;
+    int move_speed = 5;
+    sprite_inc_x = 0;
+    sprite_inc_y = 0;
+
+    if (controller_status & 0x1)
+    {
+        sprite_inc_x = -move_speed;
+    }
+    if (controller_status & 0x2)
+    {
+        sprite_inc_y = -move_speed;
+    }
+    if (controller_status & 0x4)
+    {
+        sprite_inc_y = move_speed;
+    }
+    if (controller_status & 0x8)
+    {
+        sprite_inc_x = move_speed;
+    }
+    *x_pos += sprite_inc_x;
+    *y_pos += sprite_inc_y;
+    for (int i = 0; i < 3; i++)
+    {
+        setLargeSpriteControl(i, 64, 64, *x_pos, *y_pos, i == global % 3);
+    }
+    // threadYield();
 }
